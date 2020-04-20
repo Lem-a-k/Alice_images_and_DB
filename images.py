5385700cd8a6be9d07ad4f44a85c838850037a86
Lem <lilililili@yandex.ru>
2020-04-20 17:15:19 +0400

    os.environ

--- images.py
+++ images.py
@@ -1,10 +1,13 @@
 import requests
 import json
-
-from config import OAuth_TOKEN, DIALOG_ID
-
+import os
 
 def get_size():
+    if 'OAuth_TOKEN' in os.environ:
+        OAuth_TOKEN, DIALOG_ID = os.environ['OAuth_TOKEN'], os.environ['DIALOG_ID']
+    else:
+        from config import DIALOG_ID, OAuth_TOKEN
+
     url = "https://dialogs.yandex.net/api/v1/status"
     headers = {
         'Authorization': f"OAuth {OAuth_TOKEN}"
@@ -14,6 +17,11 @@ def get_size():
 
 
 def upload_image(image_url):
+    if 'OAuth_TOKEN' in os.environ:
+        OAuth_TOKEN, DIALOG_ID = os.environ['OAuth_TOKEN'], os.environ['DIALOG_ID']
+    else:
+        from config import DIALOG_ID, OAuth_TOKEN
+
     url = f"https://dialogs.yandex.net/api/v1/skills/{DIALOG_ID}/images"
     headers = {
         'Authorization': f"OAuth {OAuth_TOKEN}",
